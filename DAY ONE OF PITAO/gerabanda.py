d658a103118fe44869e88132a067b419f70d5162
Dani Rigon <danixan@gmail.com>
2023-12-19 22:58:04 -0300

    DAY ONE OF PITAO

---
+++ DAY ONE OF PITAO/gerabanda.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("E aí parça\n")
+#2. Ask the user for the city that they grew up in.
+city = input("De onde você é?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual é o nome do seu bichinho?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Que tal sua banda chamar: "+ city + " " + pet + "?\n")
